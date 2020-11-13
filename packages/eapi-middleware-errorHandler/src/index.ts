8ee21af5a5ae0e331fe7c6f80c7bb05649524857
Jérémie Parker <github@jeremie-parker.com>
2020-11-13 21:25:02 +0100

    feat(middleware): add errorHandler middleware

---
+++ packages/eapi-middleware-errorHandler/src/index.ts
@@ -0,0 +1,22 @@
+import { Middleware } from '@p-j/eapi-types'
+
+/**
+ * Higher order function providing generic error handling for request handlers
+ */
+
+export function withErrorHandler(): Middleware {
+  return function _withErrorHandler(requestHandler) {
+    return async function errorHandler({ event, request, params }) {
+      try {
+        // await is not superfluous here as otherwise the catch is bypassed
+        return await requestHandler({ event, request, params })
+      } catch (error) {
+        // Do something with the error, for development purpose this proposes a debug param that will allow you to see the stack trace directly
+        const debug = new URL(request.url).searchParams.get('debug') === 'true'
+        return new Response(debug ? error.stack || error : null, {
+          status: 500,
+        })
+      }
+    }
+  }
+}
