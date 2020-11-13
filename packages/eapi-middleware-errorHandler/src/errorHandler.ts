9a33c7e3ade427fb616d98877aca05d5049ee39b
Jérémie Parker <github@jeremie-parker.com>
2020-11-14 00:26:35 +0100

    chore(error): rename index.ts to errorHandler.ts

--- packages/eapi-middleware-errorHandler/src/errorHandler.ts
+++ packages/eapi-middleware-errorHandler/src/errorHandler.ts
@@ -4,7 +4,7 @@ import { Middleware } from '@p-j/eapi-types'
  * Higher order function providing generic error handling for request handlers
  */
 
-export function withErrorHandler(): Middleware {
+export function withErrorHandler({ allowDebug = false } = {}): Middleware {
   return function _withErrorHandler(requestHandler) {
     return async function errorHandler({ event, request, params }) {
       try {
@@ -12,7 +12,7 @@ export function withErrorHandler(): Middleware {
         return await requestHandler({ event, request, params })
       } catch (error) {
         // Do something with the error, for development purpose this proposes a debug param that will allow you to see the stack trace directly
-        const debug = new URL(request.url).searchParams.get('debug') === 'true'
+        const debug = new URL(request.url).searchParams.get('debug') === 'true' && allowDebug
         return new Response(debug ? error.stack || error : null, {
           status: 500,
         })
