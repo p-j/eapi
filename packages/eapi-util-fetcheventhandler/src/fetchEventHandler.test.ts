8570b679b4f9f5e308beebdf79d071ba2cad59af
Jérémie Parker <github@jeremie-parker.com>
2020-11-19 00:01:23 +0100

    test(util): test default case in fetchEventHandler

--- packages/eapi-util-fetcheventhandler/src/fetchEventHandler.test.ts
+++ packages/eapi-util-fetcheventhandler/src/fetchEventHandler.test.ts
@@ -22,41 +22,53 @@ describe('fetchEventHandler', () => {
     },
   ]
 
-  it('handle a fetch event with a configured matcher', async () => {
-    const eventHandler = fetchEventHandler({
-      matcher: successfullMatcher,
-      originless: true,
-      middlewares,
+  describe('originless: true', () => {
+
+    it('handle a fetch event with a configured matcher', async () => {
+      const eventHandler = fetchEventHandler({
+        matcher: successfullMatcher,
+        originless: true,
+        middlewares,
+      })
+
+      const response = await eventHandler({} as FetchEvent)
+
+      expect(await response.text()).toBe('OK')
+      expect(response.status).toBe(200)
+      expect(response.headers.get('X-Middleware-Applied')).toBe('true')
     })
 
-    const response = await eventHandler({} as FetchEvent)
+    it('returns a 404 if the matcher returns null and originless is true', async () => {
+      const eventHandler = fetchEventHandler({
+        matcher: faillingMatcher,
+        originless: true,
+        middlewares,
+      })
 
-    expect(await response.text()).toBe('OK')
-    expect(response.status).toBe(200)
-    expect(response.headers.get('X-Middleware-Applied')).toBe('true')
-  })
+      const response = await eventHandler({} as FetchEvent)
 
-  it('returns a 404 if the matcher returns null and originless is true', async () => {
-    const eventHandler = fetchEventHandler({
-      matcher: faillingMatcher,
-      originless: true,
-      middlewares,
+      expect(await response.text()).toBe('Not Found')
+      expect(response.status).toBe(404)
+      expect(response.headers.get('X-Middleware-Applied')).toBe(null)
     })
-
-    const response = await eventHandler({} as FetchEvent)
-
-    expect(await response.text()).toBe('Not Found')
-    expect(response.status).toBe(404)
-    expect(response.headers.get('X-Middleware-Applied')).toBe(null)
   })
 
-  describe('with fetch mock', () => {
+  describe('originless: false', () => {
     beforeAll(() => {
       fetchMock.enableMocks()
     })
 
     afterAll(() => {
       fetchMock.disableMocks()
+    })
+
+    it('works fine when originless & middlewares parameters are omited', async () => {
+      const eventHandler = fetchEventHandler({ matcher: successfullMatcher })
+
+      const response = await eventHandler({} as FetchEvent)
+
+      expect(await response.text()).toBe('OK')
+      expect(response.status).toBe(200)
     })
 
     it('let the request pass through if an origin is defined and the matcher returns null', async () => {
