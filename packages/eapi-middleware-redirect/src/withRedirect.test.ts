2e6cace48accc16ace518d12f05e0d1a1fe86df0
Jérémie Parker <github@jeremie-parker.com>
2020-11-23 16:47:00 +0100

    feat(redirect): add withRedirect middlware code

---
+++ packages/eapi-middleware-redirect/src/withRedirect.test.ts
@@ -0,0 +1,148 @@
+import { withRedirect } from './withRedirect'
+import fetchMock from 'jest-fetch-mock'
+
+Response.redirect = jest.fn((url, status) => {
+  return new Response(
+    null,
+    Object.assign(
+      {
+        headers: {
+          location: new URL(url).toString(),
+        },
+        status,
+      },
+      { url },
+    ),
+  )
+})
+
+const request = new Request('https://toto.com/titi')
+
+const transform: Transform = ({ request }) => {
+  const url = new URL(request.url)
+  url.hostname = url.hostname.replace('toto', 'tata')
+  return new Request(url.toString(), request)
+}
+
+const passThrough: RequestHandler = async ({ request }) => {
+  const response = await fetch(request)
+  // Mock the redirect='follow' behavior
+  const url = response.headers.get('location')
+  if (request.redirect === 'follow' && url) {
+    return passThrough({ request: new Request(url, request) } as RequestContext)
+  }
+  return response
+}
+
+describe('Redirect', () => {
+  beforeAll(() => {
+    fetchMock.enableMocks()
+  })
+  beforeEach(() => {
+    fetchMock.resetMocks()
+  })
+  afterAll(() => {
+    fetchMock.disableMocks()
+  })
+
+  it('should perform a transparent redirect by url', async () => {
+    fetchMock.mockResponseOnce('ok', { status: 200, url: 'https://toto.com/tutu' })
+    const requestHandler = withRedirect({ transparent: true, urlOrPath: '/tutu' })(passThrough)
+    const response = await requestHandler({ request: request.clone() } as RequestContext)
+
+    expect(response).toBeInstanceOf(Response)
+    expect(response.status).toBe(200)
+    expect(response.url).toBe('https://toto.com/tutu')
+    expect(await response.text()).toBe('ok')
+  })
+
+  it('should perform a transparent redirect with transform', async () => {
+    fetchMock.mockResponseOnce('ok', { status: 200, url: 'https://tata.com/titi' })
+    const requestHandler = withRedirect({ transparent: true, transform })(passThrough)
+    const response = await requestHandler({ request: request.clone() } as RequestContext)
+
+    expect(response).toBeInstanceOf(Response)
+    expect(response.status).toBe(200)
+    expect(response.url).toBe('https://tata.com/titi')
+    expect(await response.text()).toBe('ok')
+  })
+
+  it('should perform a permanent redirect by url', async () => {
+    const requestHandler = withRedirect({ transparent: false, permanent: true, urlOrPath: '/tutu' })(passThrough)
+    const response = await requestHandler({ request: request.clone() } as RequestContext)
+
+    expect(response).toBeInstanceOf(Response)
+    expect(response.status).toBe(301)
+    expect(response.url).toBe('https://toto.com/tutu')
+  })
+
+  it('should perform a permanent redirect with transform', async () => {
+    const requestHandler = withRedirect({ transparent: false, permanent: true, transform })(passThrough)
+    const response = await requestHandler({ request: request.clone() } as RequestContext)
+
+    expect(response).toBeInstanceOf(Response)
+    expect(response.status).toBe(301)
+    expect(response.url).toBe('https://tata.com/titi')
+  })
+
+  it('should perform a temporary redirect by url', async () => {
+    const requestHandler = withRedirect({ transparent: false, permanent: false, urlOrPath: '/tutu' })(passThrough)
+    const response = await requestHandler({ request: request.clone() } as RequestContext)
+
+    expect(response).toBeInstanceOf(Response)
+    expect(response.status).toBe(302)
+    expect(response.url).toBe('https://toto.com/tutu')
+  })
+
+  it('should perform a temporary redirect with transform', async () => {
+    const requestHandler = withRedirect({ transparent: false, permanent: false, transform })(passThrough)
+    const response = await requestHandler({ request: request.clone() } as RequestContext)
+
+    expect(response).toBeInstanceOf(Response)
+    expect(response.status).toBe(302)
+    expect(response.url).toBe('https://tata.com/titi')
+  })
+
+  it('should not redirect when transparent is false and url is unchanged', async () => {
+    fetchMock.mockResponseOnce('ok', { status: 200, url: request.url })
+    const requestHandler = withRedirect({ transparent: false, transform: ({ request }) => request })(passThrough)
+    const response = await requestHandler({ request: request.clone() } as RequestContext)
+
+    expect(response).toBeInstanceOf(Response)
+    expect(response.status).toBe(200)
+    expect(response.url).toBe(request.url)
+  })
+
+  it('should act as a non-transparent redirect if redirect is set to manual and the origin sends a redirect', async () => {
+    fetchMock.mockResponse(async (req) => {
+      if (req.url.startsWith('https://toto.com/titi')) {
+        return { status: 301, headers: { location: 'https://tata.com/tutu' }, url: req.url }
+      } else if (req.url === 'https://tata.com/tutu') {
+        return { status: 200, body: 'ok', url: req.url }
+      } else {
+        return { status: 418, body: 'who are you?', url: req.url }
+      }
+    })
+
+    const requestHandlerWithRedirectManual = withRedirect({
+      transparent: true,
+      transform: ({ request }) => request,
+      redirect: 'manual',
+    })(passThrough)
+
+    const requestHandlerWithRedirectFollow = withRedirect({
+      transparent: true,
+      transform: ({ request }) => request,
+      redirect: 'follow',
+    })(passThrough)
+
+    const response = await requestHandlerWithRedirectManual({ request: request.clone() } as RequestContext)
+    const response2 = await requestHandlerWithRedirectFollow({ request: request.clone() } as RequestContext)
+
+    expect(response.status).toBe(301)
+    expect(response.url).toBe(request.url)
+    expect(response.headers.get('location')).toBe('https://tata.com/tutu')
+    expect(response2.status).toBe(200)
+    expect(response2.url).toBe('https://tata.com/tutu')
+  })
+})
--- packages/eapi-middleware-redirect/src/withRedirect.ts
+++ packages/eapi-middleware-redirect/src/withRedirect.ts
@@ -1,1 +1,52 @@
-export function withRedirect() {}
+export interface WithRedirectOptions {
+  urlOrPath?: string
+  transparent?: boolean
+  transform?: Transform
+  permanent?: boolean
+  redirect?: 'follow' | 'error' | 'manual' | undefined
+}
+
+/**
+ * @param options
+ * @param options.urlOrPath the url, absolute or relative to redirect to. Defaults to an empty string
+ * @param options.transform transformation function
+ * @param options.transparent is the redirection transparent (forward directly the targeted response). Defaults to true.
+ * @param options.permanent if not transparent, is the redirection permanent (301) or not (302). Defaults to false.
+ * @param options.redirect one of Request.redirect valid values (follow, manual, error) determining the behavior of the request handler if it encounters a redirect. Defaults to 'follow'.
+ * @returns a Middleware that will apply redirections/rewrite to the passed handler, finally returning a Response (or Promise of a Response) that is either a redirect to the new URL if args.transparent=false or an actual Response.
+ */
+export function withRedirect({
+  urlOrPath = '',
+  transform,
+  transparent = true,
+  permanent = false,
+  redirect = 'follow',
+}: WithRedirectOptions = {}): Middleware {
+  return function _withRedirect(requestHandler) {
+    return async function redirectHandler({ event, request, params }) {
+      const originalURL = new URL(request.url)
+      originalURL.searchParams.sort()
+
+      // new URL(input[, base])
+      // input <string> The absolute or relative input URL to parse.
+      // If input is relative, then base is required.
+      // If input is absolute, the base is ignored.
+      const url = new URL(urlOrPath, request.url)
+      const intermediateRequest = new Request(url.toString(), request)
+      const transformedRequest =
+        typeof transform === 'function'
+          ? transform({ request: intermediateRequest, params, event })
+          : intermediateRequest
+      const finalRequest = new Request(transformedRequest, { redirect })
+      const finalURL = new URL(finalRequest.url)
+      finalURL.searchParams.sort()
+
+      // if not transparent and the URL has been changed, the requestHandler will be ignored
+      // as we need to redirect to the new URL
+      if (!transparent && originalURL.toString() !== finalURL.toString())
+        return Response.redirect(finalURL.toString(), permanent ? 301 : 302)
+
+      return requestHandler({ event, request: finalRequest, params })
+    }
+  }
+}
