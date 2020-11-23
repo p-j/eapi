0117c130eac110d085f612cdf77083cf0886e3b8
Jérémie Parker <github@jeremie-parker.com>
2020-11-23 15:58:12 +0100

    feat(cors): handle more CORS headers
    - add Access-Control-Allow-Credentials
    - add Access-Control-Expose-Headers
    - add sensible defaults
    - change the behavior when the Origin header is not present

--- packages/eapi-middleware-cors/src/withCors.test.ts
+++ packages/eapi-middleware-cors/src/withCors.test.ts
@@ -1,41 +1,71 @@
 import { cors, withCors } from './withCors'
 
+const defaultAccessControlAllowOrigin = '*'
+const defaultAccessControlAllowHeaders = ['Origin', 'Content-Type', 'Accept', 'Authorization'].join(', ')
+const defaultAccessControlAllowMethods = ['GET', 'OPTIONS', 'HEAD'].join(', ')
+const defaultAccessControlMaxAge = '3600'
+
 const accessControlAllowHeaders = ['Accept', 'Authorization', 'Cache-Control', 'Content-Type', 'Origin', 'User-Agent']
 const accessControlAllowMethods = ['GET', 'POST', 'OPTIONS', 'DELETE', 'PUT', 'PATCH']
 const accessControlMaxAge = 86400
 
 describe('Cors', () => {
   describe('main method', () => {
-    it('should set cors headers on a given response', () => {
-      const testResponse = new Response(undefined, { status: 204 })
+    it('should set default values if no config is given', () => {
+      const response = cors({ response: new Response(undefined, { status: 200 }) })
+      expect(response.headers.get('Access-Control-Allow-Origin')).toBe(defaultAccessControlAllowOrigin)
+      expect(response.headers.get('Access-Control-Allow-Headers')).toBe(defaultAccessControlAllowHeaders)
+      expect(response.headers.get('Access-Control-Allow-Methods')).toBe(defaultAccessControlAllowMethods)
+      expect(response.headers.get('Access-Control-Max-Age')).toBe(defaultAccessControlMaxAge)
+      expect(response.headers.get('Vary')).toBe(null)
+    })
+
+    it('should set Access-Control-Allow-Credentials', () => {
+      const response = cors({ response: new Response(undefined, { status: 200 }), accessControlAllowCredentials: true })
+      expect(response.headers.get('Access-Control-Allow-Credentials')).toBe('true')
+    })
+
+    it('should set Access-Control-Expose-Headers', () => {
       const response = cors({
-        response: testResponse,
-        origin: 'https://tata.com',
+        response: new Response(undefined, { status: 200 }),
+        accessControlExposeHeaders: ['Content-Length', 'Authorization'],
+      })
+      const response2 = cors({
+        response: new Response(undefined, { status: 200 }),
+        accessControlExposeHeaders: ['*', 'Authorization'],
+      })
+      expect(response.headers.get('Access-Control-Expose-Headers')).toBe('Content-Length, Authorization')
+      expect(response2.headers.get('Access-Control-Expose-Headers')).toBe('*, Authorization')
+    })
+
+    it('should set cors headers on a given response', () => {
+      const response = cors({
+        response: new Response(undefined, { status: 200 }),
+        accessControlAllowOrigin: 'https://tata.com',
         accessControlAllowHeaders,
         accessControlAllowMethods,
         accessControlMaxAge,
       })
-      expect(response.headers.get('access-control-allow-origin')).toBe('https://tata.com')
-      expect(response.headers.get('access-control-allow-headers')).toBe(accessControlAllowHeaders.join(', '))
-      expect(response.headers.get('access-control-allow-methods')).toBe(accessControlAllowMethods.join(', '))
-      expect(response.headers.get('access-control-max-age')).toBe(`${accessControlMaxAge}`)
+      expect(response.headers.get('Access-Control-Allow-Origin')).toBe('https://tata.com')
+      expect(response.headers.get('Access-Control-Allow-Headers')).toBe(accessControlAllowHeaders.join(', '))
+      expect(response.headers.get('Access-Control-Allow-Methods')).toBe(accessControlAllowMethods.join(', '))
+      expect(response.headers.get('Access-Control-Max-Age')).toBe(String(accessControlMaxAge))
       expect(response.headers.get('Vary')).toBe('Origin')
     })
 
     it('should not set cors headers on a given response if the origin is not allowed', () => {
-      const testResponse = new Response(undefined, { status: 204 })
       const response = cors({
-        response: testResponse,
-        origin: 'https://tata.com',
+        response: new Response(undefined, { status: 200 }),
+        accessControlAllowOrigin: 'https://tata.com',
         isOriginAllowed: (origin: string) => origin !== 'https://tata.com',
         accessControlAllowHeaders,
         accessControlAllowMethods,
         accessControlMaxAge,
       })
-      expect(response.headers.get('access-control-allow-origin')).toBe(null)
-      expect(response.headers.get('access-control-allow-headers')).toBe(null)
-      expect(response.headers.get('access-control-allow-methods')).toBe(null)
-      expect(response.headers.get('access-control-max-age')).toBe(null)
+      expect(response.headers.get('Access-Control-Allow-Origin')).toBe(null)
+      expect(response.headers.get('Access-Control-Allow-Headers')).toBe(null)
+      expect(response.headers.get('Access-Control-Allow-Methods')).toBe(null)
+      expect(response.headers.get('Access-Control-Max-Age')).toBe(null)
     })
   })
 
@@ -47,18 +77,14 @@ describe('Cors', () => {
       const request = new Request('https://tutu.com', { headers: { Origin: 'https://tata.com' } })
       const event = new FetchEvent('fetch', { request })
 
-      const requestHandler = withCors({
-        accessControlAllowHeaders,
-        accessControlAllowMethods,
-        accessControlMaxAge,
-      })(testHandler)
+      const requestHandler = withCors()(testHandler)
 
       const response = await requestHandler({ event, request, params: {} })
 
-      expect(response.headers.get('access-control-allow-origin')).toBe('https://tata.com')
-      expect(response.headers.get('access-control-allow-headers')).toBe(accessControlAllowHeaders.join(', '))
-      expect(response.headers.get('access-control-allow-methods')).toBe(accessControlAllowMethods.join(', '))
-      expect(response.headers.get('access-control-max-age')).toBe(`${accessControlMaxAge}`)
+      expect(response.headers.get('Access-Control-Allow-Origin')).toBe('https://tata.com')
+      expect(response.headers.get('Access-Control-Allow-Headers')).toBe(defaultAccessControlAllowHeaders)
+      expect(response.headers.get('Access-Control-Allow-Methods')).toBe(defaultAccessControlAllowMethods)
+      expect(response.headers.get('Access-Control-Max-Age')).toBe(defaultAccessControlMaxAge)
       expect(response.headers.get('Vary')).toBe('Origin')
     })
 
@@ -78,10 +104,10 @@ describe('Cors', () => {
 
       const response = await requestHandler({ event, request, params: {} })
 
-      expect(response.headers.get('access-control-allow-origin')).toBe('https://tata.com')
-      expect(response.headers.get('access-control-allow-headers')).toBe(accessControlAllowHeaders.join(', '))
-      expect(response.headers.get('access-control-allow-methods')).toBe(accessControlAllowMethods.join(', '))
-      expect(response.headers.get('access-control-max-age')).toBe(`${accessControlMaxAge}`)
+      expect(response.headers.get('Access-Control-Allow-Origin')).toBe('https://tata.com')
+      expect(response.headers.get('Access-Control-Allow-Headers')).toBe(accessControlAllowHeaders.join(', '))
+      expect(response.headers.get('Access-Control-Allow-Methods')).toBe(accessControlAllowMethods.join(', '))
+      expect(response.headers.get('Access-Control-Max-Age')).toBe(String(accessControlMaxAge))
       expect(response.headers.get('Vary')).toBe('Origin')
     })
 
@@ -100,10 +126,27 @@ describe('Cors', () => {
       })(testHandler)
 
       const response = await requestHandler({ event, request, params: {} })
-      expect(response.headers.get('access-control-allow-origin')).toBe(null)
-      expect(response.headers.get('access-control-allow-headers')).toBe(null)
-      expect(response.headers.get('access-control-allow-methods')).toBe(null)
-      expect(response.headers.get('access-control-max-age')).toBe(null)
+      expect(response.headers.get('Access-Control-Allow-Origin')).toBe(null)
+      expect(response.headers.get('Access-Control-Allow-Headers')).toBe(null)
+      expect(response.headers.get('Access-Control-Allow-Methods')).toBe(null)
+      expect(response.headers.get('Access-Control-Max-Age')).toBe(null)
+    })
+
+    it('bypass the middleware without origin', async () => {
+      async function testHandler() {
+        return new Response('OK', { status: 200 })
+      }
+      const request = new Request('https://tutu.com')
+      const event = new FetchEvent('fetch', { request })
+
+      const requestHandler = withCors()(testHandler)
+
+      const response = await requestHandler({ event, request, params: {} })
+
+      expect(response.headers.get('Access-Control-Allow-Origin')).toBe(null)
+      expect(response.headers.get('Access-Control-Allow-Headers')).toBe(null)
+      expect(response.headers.get('Access-Control-Allow-Methods')).toBe(null)
+      expect(response.headers.get('Access-Control-Max-Age')).toBe(null)
     })
   })
 })
--- packages/eapi-middleware-cors/src/withCors.ts
+++ packages/eapi-middleware-cors/src/withCors.ts
@@ -1,64 +1,100 @@
-import { Middleware } from '@p-j/eapi-types'
-
 export interface WithCorsOptions {
   isOriginAllowed?: Function
-  accessControlAllowHeaders: string[]
-  accessControlAllowMethods: string[]
-  accessControlMaxAge: number
+  accessControlAllowHeaders?: string[]
+  accessControlAllowMethods?: string[]
+  accessControlMaxAge?: number
+  accessControlAllowCredentials?: boolean
+  accessControlExposeHeaders?: string[]
 }
 
 export interface CorsOptions extends WithCorsOptions {
   response: Response
-  origin: string
+  accessControlAllowOrigin?: string
 }
 
 /**
+ * Uitility function to apply CORS configuration to a Response
  * @param options
  * @param options.response response on which apply cors headers
- * @param options.origin origin to set for access-control-allow-origin
  * @param options.isOriginAllowed an optional function to validate the origin of the request
+ * @param options.accessControlAllowOrigin Access-Control-Allow-Origin value. Defaults to '*'.
+ * @param options.accessControlAllowHeaders an array of allowed headers. Defaults to ['origin', 'content-type', 'accept', 'authorization']
+ * @param options.accessControlAllowMethods an array of allowed methods. Defaults to ['GET', 'OPTIONS', 'HEAD']
+ * @param options.accessControlMaxAge how long the results of a preflight request can be cached. Defaults to 1 hour.
+ * @param options.accessControlAllowCredentials control the Access-Control-Allow-Credentials header.
+ * @param options.accessControlExposeHeaders an array of exposed headers. Defaults to [].
  * @returns a Response with CORS headers
  */
 export function cors({
   response,
-  origin,
   isOriginAllowed,
-  accessControlAllowHeaders,
-  accessControlAllowMethods,
-  accessControlMaxAge,
+  accessControlAllowOrigin = '*',
+  accessControlAllowHeaders = ['Origin', 'Content-Type', 'Accept', 'Authorization'],
+  accessControlAllowMethods = ['GET', 'OPTIONS', 'HEAD'],
+  accessControlMaxAge = 3600,
+  accessControlAllowCredentials = false,
+  accessControlExposeHeaders = [],
 }: CorsOptions): Response {
-  if (typeof isOriginAllowed === 'function' && !isOriginAllowed(origin)) {
+  if (typeof isOriginAllowed === 'function' && !isOriginAllowed(accessControlAllowOrigin)) {
     return response
   }
+
   const responseWithCors = new Response(response.body, response)
-  responseWithCors.headers.set('Access-Control-Allow-Origin', origin)
+  responseWithCors.headers.set('Access-Control-Allow-Origin', accessControlAllowOrigin)
   responseWithCors.headers.set('Access-Control-Allow-Headers', accessControlAllowHeaders.join(', '))
   responseWithCors.headers.set('Access-Control-Allow-Methods', accessControlAllowMethods.join(', '))
-  responseWithCors.headers.set('Access-Control-Max-Age', `${accessControlMaxAge}`)
+  responseWithCors.headers.set('Access-Control-Max-Age', String(accessControlMaxAge))
+
+  if (accessControlAllowCredentials) {
+    responseWithCors.headers.set('Access-Control-Allow-Credentials', 'true')
+  }
+
+  if (accessControlExposeHeaders.length) {
+    responseWithCors.headers.set('Access-Control-Expose-Headers', accessControlExposeHeaders.join(', '))
+  }
+
   const vary = responseWithCors.headers.get('Vary')
-  if (!vary || !vary.includes('Origin')) {
+  if (accessControlAllowOrigin !== '*' && (!vary || !vary.includes('Origin'))) {
     responseWithCors.headers.append('Vary', 'Origin')
   }
+
   return responseWithCors
 }
 
+/**
+ * Middleware factory to apply CORS configuration
+ * @param options
+ * @param options.isOriginAllowed an optional function to validate the origin of the request
+ * @param options.accessControlAllowHeaders an array of allowed headers. Defaults to ['origin', 'content-type', 'accept', 'authorization']
+ * @param options.accessControlAllowMethods an array of allowed methods. Defaults to ['GET', 'OPTIONS', 'HEAD']
+ * @param options.accessControlMaxAge how long the results of a preflight request can be cached. Defaults to 1 hour.
+ * @param options.accessControlAllowCredentials control the Access-Control-Allow-Credentials header.
+ * @param options.accessControlExposeHeaders an array of exposed headers. Defaults to [].
+ * @returns a Middleware function
+ */
 export function withCors({
   isOriginAllowed,
   accessControlAllowHeaders,
   accessControlAllowMethods,
   accessControlMaxAge,
-}: WithCorsOptions): Middleware {
-  return function _withCors(requestHandler) {
-    return async function corsHandler({ event, request, params }) {
+  accessControlAllowCredentials,
+  accessControlExposeHeaders,
+}: WithCorsOptions = {}): Middleware {
+  return function _withCors(requestHandler: RequestHandler) {
+    return async function corsHandler({ event, request, params }: RequestContext) {
       const response = await requestHandler({ event, request, params })
-      const origin = request.headers.get('Origin') || '*'
+      const origin = request.headers.get('Origin')
+      // If the Origin header is missing we don't add CORS Headers
+      if (!origin) return response
       return cors({
         response,
-        origin,
         isOriginAllowed,
+        accessControlAllowOrigin: origin,
         accessControlAllowHeaders,
         accessControlAllowMethods,
         accessControlMaxAge,
+        accessControlAllowCredentials,
+        accessControlExposeHeaders,
       })
     }
   }
